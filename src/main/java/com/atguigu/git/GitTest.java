28d41dcc6ff4c71001b7f7185399db857d2d0ab2
YanHao <yh_42_edu@163.com>
2022-06-28 13:44:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author yanhao
+ * @note
+ * @create 2022-06-28 下午 1:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
